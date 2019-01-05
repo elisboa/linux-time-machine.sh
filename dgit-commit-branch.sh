d187f56701392b14fd9309666b8a6e2e232eee20
Eduardo Lisboa <eduardo.lisboa@gmail.com>
2019-01-04 23:02:50 -0200

    - removed lines that create and initialize the git repository

--- dgit-commit-branch.sh
+++ dgit-commit-branch.sh
@@ -7,24 +7,12 @@
 # set some environment vars and aliases
 function set-env () {
 	
+	LANG=C
+
 	# creates an alias to dgit, so we can use dgit instead of git to access our customized git environment
 	alias dgit="git --git-dir $HOME/.dotfiles/.git --work-tree $HOME"
 
-	# if there is no $HOME/dotfiles, create it 
-	mkdir -pv $HOME/.dotfiles
-
-	if [[ -d $HOME/.dotfiles]] && [[ ! -d $HOME/.dotfiles/.git ]]
-		cd $HOME/.dotfiles
-		git init .
-		echo "*" > .gitignore
-		git add -f .gitignore
-		git commit -m "gitignore added with * entry"
-		cd $HOME
-		dgit reset --hard
-		dgit status
-	fi
 }
-
 # check environment requirements
 function check-env () {
 	
