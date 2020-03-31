670d2f33f6c57d53326d602734ca7b61039a4bd4
Eduardo Lisboa <eduardo.lisboa@gmail.com>
2020-03-31 17:12:54 -0300

    - fixed remote repository reference on push-remote

--- arguments.sh
+++ arguments.sh
@@ -14,7 +14,8 @@ function push-remote () {
 		# ... and push local branches, using mirror
 		#$TMGIT push ${remote_repo} --mirror 2> /dev/null
 		#... and push local branches, using update
-		$TMGIT push -u --set-upstream origin "${CUR_BRANCH}"
+		$TMGIT push -u --set-upstream "$remote_repo" "${CUR_BRANCH}"
+		$TMGIT push -u $remote_repo HEAD
 		$TMGIT remote -v
 
 		#$TMGIT push ${remote_repo} -u --follow-tags 2> /dev/null
